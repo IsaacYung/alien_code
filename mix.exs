62b240e2e22d7a329c475e936759d7bfaae14dfd
Isaac Yung Lopes <isaaczoi@gmail.com>
2017-05-28 20:44:51 -0300

    Added cowboy and ecto

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule AlienCodeApi.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger, :plug, :poison]]
+    [extra_applications: [:logger, :plug, :poison, :ecto, :cowboy]]
   end
 
   # Dependencies can be Hex packages:
@@ -29,8 +29,10 @@ defmodule AlienCodeApi.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
+      {:cowboy, "~> 1.1"},
       {:poison, "~> 3.1"},
-      {:plug, "~> 1.3"}
+      {:plug, "~> 1.3"},
+      {:ecto, "~> 2.1"}
     ]
   end
 end
