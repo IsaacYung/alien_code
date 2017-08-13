bb430c473bd172e06de71ece1d400903230fd91d
Isaac Yung Lopes <isaaczoi@gmail.com>
2017-08-13 09:17:26 -0300

    save on database

--- lib/alien_code/controllers/author.ex
+++ lib/alien_code/controllers/author.ex
@@ -1,8 +1,26 @@
 defmodule AlienCode.Controllers.Author do
   alias AlienCode.Presenters.Response
+  alias AlienCode.Models.Author
+  alias AlienCode.Repo
+  import Ecto.Query
 
   def new(conn) do
-    IO.inspect conn.body_params
-    Response.build 200, conn.body_params
+    params = conn.body_params["author"]
+    changeset = Author.changeset(%Author{},
+    %{
+      login: params["login"],
+      email: params["email"],
+      pass_unencrypted: params["password"],
+      display_name: params["display_name"],
+      nicename: params["nicename"]
+    })
+
+    cond do
+      changeset.valid? == true ->
+        Repo.insert changeset
+        Response.build 200, conn.body_params
+      changeset.valid? == false ->
+        Response.build 400, conn.body_params
+    end
   end
 end
